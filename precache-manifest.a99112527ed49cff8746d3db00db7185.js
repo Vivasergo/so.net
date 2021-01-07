e99f083c7782a70fda01744c390ecd005917647c
Serhii Vasilikhin <vivasergo@gmail.com>
2021-01-07 12:57:29 +0100

    Updates

---
+++ precache-manifest.a99112527ed49cff8746d3db00db7185.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1831b075fbfdddcd8d5ed49ee62573e2",
+    "url": "/so.net/index.html"
+  },
+  {
+    "revision": "5615553c4a223b470f97",
+    "url": "/so.net/static/css/main.d77c1f01.chunk.css"
+  },
+  {
+    "revision": "71b82f3d21a9efb8aeb2",
+    "url": "/so.net/static/js/2.60ead7fa.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/so.net/static/js/2.60ead7fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9460606c1a4828d3617f",
+    "url": "/so.net/static/js/3.861e2413.chunk.js"
+  },
+  {
+    "revision": "5615553c4a223b470f97",
+    "url": "/so.net/static/js/main.c3ebd937.chunk.js"
+  },
+  {
+    "revision": "cff6487c80698355300f25a7ab23d444",
+    "url": "/so.net/static/js/main.c3ebd937.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eddc47287936d31a9753",
+    "url": "/so.net/static/js/runtime-main.ce9729ea.js"
+  },
+  {
+    "revision": "9e0373c2b4410c49439dfd822c5fd16e",
+    "url": "/so.net/static/media/loading_apple.9e0373c2.gif"
+  }
+]);
