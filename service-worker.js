3361566c2af24ce014bbb117506cd39c6eab5363
Serhii Vasilikhin <vivasergo@gmail.com>
2021-01-15 12:31:25 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/so.net/precache-manifest.ebe0726f9fc983ce570519ccccbc8828.js"
+  "/so.net/precache-manifest.5dc52b1647d76d3aa7ec2b28c5070b00.js"
 );
 
 self.addEventListener('message', (event) => {
